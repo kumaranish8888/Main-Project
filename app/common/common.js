5b4ecf7e4d2c6d9140289d8a8025ae10d4e4eb3e
kumaranish8888 <kumaranish8888@gmail.com>
2017-03-21 21:00:06 -0400

    Added files

--- app/common/common.js
+++ app/common/common.js
@@ -1,8 +1,10 @@
 (function(){
     
     angular.module("common", []);
-    
     angular.module("common").directive("customBrand", [customBrand]);
+    angular.module("common").directive("customNavBar", [customNavBar]);
+    angular.module("common").directive("customDatePicker", [customDatePicker]);
+    angular.module("common").directive("numbersOnly", [numbersOnly]);
     
     function customBrand(){
         return {
@@ -10,15 +12,39 @@
         };
     }
     
-    angular.module("common").directive("customNavBar", [customNavBar]);
-    
     function customNavBar() {
         
          return {
           templateUrl: 'app/header/header.tpl.html',
              restrict: "C"
         };
+    }
+    
+    function customDatePicker(){
         
+        return {
+          restrict: "A",
+            link: function(scope,element,attrs){
+                element.datepicker();
+            }
+        };
+    }
+    
+    function numbersOnly(){     
+        return {
+          restrict:"A",
+            link: function(scope, element, attrs){
+                element.bind("keypress", function(e){
+                    console.log(e);
+                        var restricted = e.key;
+                        var reg = new  RegExp(/^[0-9]+$/); 
+                        var maxlength = attrs[maxlength];
+                    if(!reg.test(restricted) || this.value.length>maxlength){
+                        e.preventDefault();
+                    }
+                })
+            }
+        };
     }
     
 })();
